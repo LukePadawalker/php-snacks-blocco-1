bad786c3f40ca95c609c9b0f1620275e58228ba7
LukePadawalker <padurariu.luca@gmail.com>
2024-02-19 19:52:02 +0200

    done logics snack 1

--- index.php
+++ index.php
@@ -6,19 +6,19 @@ $partite = [
         'squadraOspite' => 'Bologna',
         'puntiCasa' => 70,
         'puntiOspite' => 64
-    ]
+    ],
     [
         'squadraCasa' => 'Milano',
         'squadraOspite' => 'Trieste',
         'puntiCasa' => 80,
         'puntiOspite' => 76
-    ]
+    ],
     [
         'squadraCasa' => 'Belluno',
         'squadraOspite' => 'Rome',
         'puntiCasa' => 64,
         'puntiOspite' => 90
-    ]
+    ],
     [
         'squadraCasa' => 'Verona',
         'squadraOspite' => 'Modena',
@@ -26,9 +26,12 @@ $partite = [
         'puntiOspite' => 4
     ]
 
-]
+    ];
 
 
+foreach($partite as $partita)
+    echo $partita['squadraCasa'].$partita['squadraOspite'].'|'.$partita['puntiCasa'].'-'.$partita['puntiOspite']
+;
 
 ?>
 
