59cb340b8d3ae16fe08810e3e69337432f8ea438
LukePadawalker <padurariu.luca@gmail.com>
2024-02-19 18:16:17 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+$partite = [
+    [
+        'squadraCasa' => 'Torino',
+        'squadraOspite' => 'Bologna',
+        'puntiCasa' => 70,
+        'puntiOspite' => 64
+    ]
+    [
+        'squadraCasa' => 'Milano',
+        'squadraOspite' => 'Trieste',
+        'puntiCasa' => 80,
+        'puntiOspite' => 76
+    ]
+    [
+        'squadraCasa' => 'Belluno',
+        'squadraOspite' => 'Rome',
+        'puntiCasa' => 64,
+        'puntiOspite' => 90
+    ]
+    [
+        'squadraCasa' => 'Verona',
+        'squadraOspite' => 'Modena',
+        'puntiCasa' => 350,
+        'puntiOspite' => 4
+    ]
+
+]
+
+
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
